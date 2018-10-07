e5bd60129b167c25f81598a9278a3b3c8ec9f5dd
Bennett Haase-Divine <b.haase-divine@ku.edu>
2018-10-07 01:36:58 -0500

    Added miniGame integration

--- sweeper_UI.py
+++ sweeper_UI.py
@@ -9,6 +9,9 @@ import math
 from Board import *
 from Cell import Cell
 from gamefunctions import *
+#from mini_games.SimpleMiniGame import *
+
+
 
 #global maxes/mins/button sizes
 button_width = 80
@@ -144,6 +147,7 @@ class minesweeper_gui:
         """
         cheat_mode=False
 
+
         pygame.init()
         pygame.font.init()
 
@@ -197,6 +201,7 @@ class minesweeper_gui:
                     sys.exit()
 
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    #Reveal tile
                     mouse_pos = pygame.mouse.get_pos()
                     m_rect = pygame.rect.Rect(mouse_pos, (1, 1))
 
@@ -218,12 +223,23 @@ class minesweeper_gui:
                                         mine_hit = reveal(gB,cell.y,cell.x)
 
                                         if(mine_hit):
-                                            for row in range(rows):
-                                                for cell in cell_list[row]:
-                                                    cell.m_cell.set_revealed()
-                                            update_display(gameDisplay, gB, cell_list, flagged_count)
-                                            game_over(gameDisplay)
-
+                                            #TODO make several mini games
+                                            mini_game_win=simple_game(gameDisplay);
+                                            if(mini_game_win):
+                                                #Won miniGame
+                                                mine_hit=False;
+                                                gB.board[cell.y][cell.x].isRevealed=False;
+                                                flags += cell.m_cell.set_flag()
+                                                flags_button.text = "Flags remaining: " + str(flags)
+                                            elif(not mini_game_win):
+                                                #Lose Mini Game
+                                                for row in range(rows):
+                                                    for cell in cell_list[row]:
+                                                        cell.m_cell.set_revealed()
+                                                update_display(gameDisplay, gB, cell_list, flagged_count,flags_button,quit_button)
+                                                game_over(gameDisplay)
+                                                pygame.quit()
+                                                sys.exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
                     #flag on right click
                     mouse_pos = pygame.mouse.get_pos()
@@ -251,11 +267,11 @@ class minesweeper_gui:
                 elif event.type==pygame.KEYDOWN and event.key == 304:
                 #Cheat mode
                     cheat_mode = not cheat_mode
-                if (game_win == False):
-                    if not cheat_mode:
-                        flagged_count = update_display(gameDisplay, gB, cell_list, flagged_count)
-                    else:
-                        flagged_count = update_display_cheat(gameDisplay, gB, cell_list, flagged_count)
+                if (game_win == False): #Print display
+                    if not cheat_mode: #Print normal board
+                        flagged_count = update_display(gameDisplay, gB, cell_list, flagged_count,flags_button,quit_button)
+                    else: #Print cheat board
+                        flagged_count = update_display_cheat(gameDisplay, gB, cell_list, flagged_count,flags_button,quit_button)
 
                 if(flagged_count == mines):
 
@@ -263,7 +279,7 @@ class minesweeper_gui:
                     for row in range(rows):
                         for cell in cell_list[row]:
                             cell.m_cell.set_revealed()
-                    flagged_count = update_display(gameDisplay, gB, cell_list, flagged_count)
+                    flagged_count = update_display(gameDisplay, gB, cell_list, flagged_count,flags_button,quit_button)
                     pygame.font.init()
                     menu_but = gui_button((0,204,0),0,0, display_width-button_width,40, "YOU WIN!")
                     menu_but.draw(gameDisplay, 1)
@@ -277,12 +293,13 @@ class minesweeper_gui:
 
             pygame.display.update()
 
-def update_display(display, gB, cell_list,flagged_count):
+def update_display(display, gB, cell_list,flagged_count,flags_button,quit_button):
     """
     @pre take display, game board, cell 2d array, and flagged count inputs
     @post update each game cell
     @return flagged count for game win checking
     """
+    display.fill((0,0,0))
     flagged_count = 0
     for row in range(gB.get_height()):
         for cell in cell_list[row]:
@@ -297,18 +314,25 @@ def update_display(display, gB, cell_list,flagged_count):
                     flagged_count += 1
             else:
                 display.blit(cell_image, (cell.x * cell_size, 40 + cell.y * cell_size))
+    flags_button.draw(display, 1)
+    quit_button.draw(display, 1)
     return (flagged_count)
 
-def update_display_cheat(display, gB, cell_list,flagged_count):
+def update_display_cheat(display, gB, cell_list,flagged_count,flags_button,quit_button):
     """
     @pre take display, game board, cell 2d array, and flagged count inputs
     @post update each game cell displaying cheat info
     @return flagged count for game win checking
     """
+    display.fill((0, 0, 0))
     flagged_count = 0
     for row in range(gB.get_height()):
         for cell in cell_list[row]:
             display.blit(cell_contents[gB.board[cell.y][cell.x].get_cell_textRep()], (cell.x * cell_size, 40 + cell.y * cell_size))
+    if cell.m_cell.isFlagged and cell.m_cell.isMined:
+        flagged_count += 1
+    flags_button.draw(display, 1)
+    quit_button.draw(display, 1)
     return (flagged_count)
 
 def start_game(rows,cols,mines):
@@ -322,3 +346,26 @@ def start_game(rows,cols,mines):
     place_mines(gB,mines)
     board_create(gB)
     ms.gui_start(gB,rows, cols, mines)
+
+def simple_game(Surface):
+    mini_game_size = Surface.get_width(), Surface.get_height()
+    win_img=pygame.image.load("img/win_tile.png")
+    lose_img=pygame.image.load("img/lose_tile.png")
+
+    gameSurface = pygame.Surface(mini_game_size)
+    gameSurface.blit(win_img, (0,0))
+    gameSurface.blit(lose_img, (120,0))
+    Surface.blit(gameSurface, (0,0))
+    pygame.display.flip()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1:
+                mx, my = pygame.mouse.get_pos()
+                if mx <= 120:
+                    return True
+                else:
+                    return False
+        pygame.display.update()
